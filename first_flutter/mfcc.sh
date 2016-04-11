b6346754b7dcb63d6a1a0baca7b7aa9c0746e6e2
Hiroki Mori <yamori813@yahoo.co.jp>
2016-04-11 11:27:46 +0900

    fixed mfcc option

--- first_flutter/mfcc.sh
+++ first_flutter/mfcc.sh
@@ -2,4 +2,4 @@
 
 SPTKBIN="../../SPTK-3.7/bin"
 
-./center.sh $1 | ${SPTKBIN}/mfcc/mfcc -l 1024 -f 44.1 -m 16 -n 20 -a 0.97 
+./center.sh $1 | ${SPTKBIN}/mfcc/mfcc -l 1024 -s 44.1 -m 16 -n 20 -a 0.97 
