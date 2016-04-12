e33924f5318c5b7058db4be655c698257ef58e38
Hiroki Mori <yamori813@yahoo.co.jp>
2016-04-12 16:04:06 +0900

    add delta classifier

---
+++ first_flutter/train_delta.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+host = "127.0.0.1"
+port = 9199
+name = "mfcc_test"
+FRAMES = 8
+
+require 'jubatus/classifier/client'
+
+client = Jubatus::Classifier::Client::Classifier.new(host, port, name)
+
+train_data = []
+
+open("list.csv") do |file|
+    while l = file.gets
+        array = l.chomp.split(",")
+        IO.popen("./delta.sh " << FRAMES.to_s << " " << array[0], "r+") {|io|
+            io.close_write
+            lpc = io.read(16*4*FRAMES*3)
+            x = lpc.unpack("f*")
+            mfcc = {}
+            for num in 0..15 do
+              sum = 0
+              for frame in 0..FRAMES-1 do
+                sum = sum + x[num + frame * 16 * 3]
+              end
+              mfcc["a" << num.to_s] = sum / FRAMES
+            end
+            max = mfcc.max { |a, b| a[1] <=> b[1] }
+            min = mfcc.min { |a, b| a[1] <=> b[1] }
+            for num in 0..15 do
+              val = mfcc["a" << num.to_s]
+              mfcc["a" << num.to_s] = (val - min[1]) / (max[1] - min[1])
+            end
+            delta = {}
+            for num in 0..15 do
+              sum = 0
+              for frame in 0..FRAMES-2 do
+                sum = sum + x[num + 16 + frame * 16 * 3].abs
+              end
+              delta["d" << num.to_s] = sum / (FRAMES - 1)
+            end
+            max = delta.max { |a, b| a[1] <=> b[1] }
+            min = delta.min { |a, b| a[1] <=> b[1] }
+            for num in 0..15 do
+              val = delta["d" << num.to_s]
+              delta["d" << num.to_s] = (val - min[1]) / (max[1] - min[1])
+            end
+            mfcc.update(delta)
+            train_data.push [array[1], Jubatus::Common::Datum.new(mfcc)]
+        }
+    end
+end
+
+client.train(train_data)
+
