30912ccd56381b5d7f6d390640ca513416589d1a
Hiroki Mori <yamori813@yahoo.co.jp>
2016-04-13 16:57:34 +0900

    add comment

--- first_flutter/dmpzero.sh
+++ first_flutter/dmpzero.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 SPTKBIN="../../SPTK-3.7/bin"
-SAMPLE=2205
+
+# 0.05sec at 44100K
+SAMPLE=2205 
 
 ${SPTKBIN}/x2x/x2x +sf temp.raw | ${SPTKBIN}/zcross/zcross -l ${SAMPLE} | ${SPTKBIN}/dmp/dmp +f 
