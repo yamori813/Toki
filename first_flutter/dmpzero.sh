554c31c9f29a736bbcb9f430e97f7fa99e1cf20f
Hiroki Mori <yamori813@yahoo.co.jp>
2016-04-19 17:22:53 +0900

    calc time

--- first_flutter/dmpzero.sh
+++ first_flutter/dmpzero.sh
@@ -3,6 +3,6 @@
 SPTKBIN="../../SPTK-3.7/bin"
 
 # 0.05sec at 44100K
-SAMPLE=2205 
+SAMPLE=2205
 
-${SPTKBIN}/x2x/x2x +sf temp.raw | ${SPTKBIN}/zcross/zcross -l ${SAMPLE} | ${SPTKBIN}/dmp/dmp +f 
+${SPTKBIN}/x2x/x2x +sf temp.raw | ${SPTKBIN}/zcross/zcross -l ${SAMPLE} | ${SPTKBIN}/dmp/dmp +f | awk '{print $1*0.05 " " $2}'
