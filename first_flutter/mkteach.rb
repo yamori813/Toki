207c3de8aa66ebf08fd78c302f9a9985e2b68a41
Hiroki Mori <yamori813@yahoo.co.jp>
2016-04-21 08:01:04 +0900

    use pitch command make teach data

--- first_flutter/mkteach.rb
+++ first_flutter/mkteach.rb
@@ -1,27 +1,33 @@
 inpath = ARGV[0]
 outpath = ARGV[1]
 
+interval = 0.8
+
 count = 0
 Dir::glob( inpath + "/*" ).each {|fname|
   if(system("sox " << fname << " temp.raw >/dev/null 2>&1"))
-    IO.popen("./dmpzero.sh" , "r+") {|io|
+    IO.popen("./pitch.sh" , "r+") {|io|
       io.close_write
       arr = []
       while(str=io.gets)
         str.chomp!
-        val = str.split("\t")
-        arr.push(val[1].to_i)
+        val = str.split(" ")
+        arr.push(val)
       end
-      num = 5
-      while(num < arr.length - 5) 
-          if arr[num] > 10 && arr[num - 1] * 1.2 < arr[num] && arr[num] > arr[num + 1] * 1.2 then
-          pos = (num * 0.05 - 0.2).round(2)
+      time = arr[1][0].to_f - arr[0][0].to_f
+      num = (0.2 / time).to_i
+      while(num < arr.length - (0.2 / time).to_i) 
+        if arr[num][1].to_f != 0
+          pos = arr[num][0].to_f.round(2) - 0.2
           system("sox " << fname << " " << outpath << "/S" << "%04d" % [count] << ".aiff trim " << pos.to_s << " 0.4")
           puts fname + ",S%04d.aiff" % [count] + ",%.2f" % pos
           count = count + 1
-          num = num + 10
+          num = num + interval / time
         else
           num = num + 1
+        end
+        if count > 100
+          break
         end
       end
     }
--- first_flutter/pitch.sh
+++ first_flutter/pitch.sh
@@ -2,5 +2,4 @@
 
 SPTKBIN="../../SPTK-3.7/bin"
 
-#./center.sh $1 | ${SPTKBIN}/fftr/fftr -l 1024 -A | ${SPTKBIN}/dmp/dmp +f | head -512
-${SPTKBIN}/x2x/x2x +sf temp.raw | ${SPTKBIN}/pitch/pitch -a 1 -s 44.1 -p 80 -L 1000 -H 10000 | ${SPTKBIN}/dmp/dmp +f | awk '{print $1 * 80.0 / 44100 " " $2}'
+${SPTKBIN}/x2x/x2x +sf temp.raw | ${SPTKBIN}/pitch/pitch -a 1 -t 0.2 -s 44.1 -p 80 -L 1000 -H 10000 -o 1 | ${SPTKBIN}/dmp/dmp +f | awk '{print $1 * 80.0 / 44100 " " $2}'
