7a2552e97bacaf9224fd4189b0dd1ae942e0fe58
Hiroki Mori <yamori813@yahoo.co.jp>
2016-04-22 10:35:48 +0900

    not use delta vector

--- first_flutter/test.rb
+++ first_flutter/test.rb
@@ -3,29 +3,37 @@
 host = "127.0.0.1"
 port = 9199
 name = "mfcc_test"
+FRAMES = 8
 
 require 'jubatus/classifier/client'
 
 client = Jubatus::Classifier::Client::Classifier.new(host, port, name)
 
+testsnd = []
+
+if ARGV[0] != nil 
+testsnd = [ARGV[0]]
+else
 testsnd = [
-"snd/T30.aiff",
 "snd/T40.aiff",
 "snd/T8.aiff",
+"snd/T30.aiff",
 "snd/T68.aiff"
 ]
+end
 
 testsnd.each{|file|
-    IO.popen("./mfcc.sh " << file, "r+") {|io|
+    IO.popen("./delta.sh " << FRAMES.to_s << " " << file, "r+") {|io|
         io.close_write
-        lpc = io.read(16*4)
-        x = []
+        lpc = io.read(16*4*FRAMES*3)
         x = lpc.unpack("f*")
-        hash = {}
+        mfcc = {}
         for num in 0..15 do
-            hash["a" << num.to_s] = x[num]
+          for frame in 0..FRAMES-1 do
+            mfcc["a" << frame.to_s << num.to_s] = x[num + frame * 16 * 3]
+          end
         end
-        results = client.classify [Jubatus::Common::Datum.new(hash)]
+        results = client.classify [Jubatus::Common::Datum.new(mfcc)]
         puts file + " " + results[0].max_by{ |x| x.score }.label 
 #        results.each { |result|
 #            result.each { |r|
--- first_flutter/train.rb
+++ first_flutter/train.rb
@@ -3,6 +3,7 @@
 host = "127.0.0.1"
 port = 9199
 name = "mfcc_test"
+FRAMES = 8
 
 require 'jubatus/classifier/client'
 
@@ -10,19 +11,32 @@ client = Jubatus::Classifier::Client::Classifier.new(host, port, name)
 
 train_data = []
 
-open("list.csv") do |file|
+listfile = ""
+
+if ARGV[0] != nil
+  listfile = ARGV[0]
+else
+  listfile = "list.csv"
+end
+
+open(listfile) do |file|
     while l = file.gets
         array = l.chomp.split(",")
-        IO.popen("./mfcc.sh " << array[0], "r+") {|io|
+        if array[1] != nil
+p array[1]
+        IO.popen("./delta.sh " << FRAMES.to_s << " " << array[0], "r+") {|io|
             io.close_write
-            lpc = io.read(16*4)
+            lpc = io.read(16*4*FRAMES*3)
             x = lpc.unpack("f*")
-            hash = {}
+            mfcc = {}
             for num in 0..15 do
-                hash["a" << num.to_s] = x[num]
+              for frame in 0..FRAMES-1 do
+                mfcc["a" << frame.to_s << num.to_s] = x[num + frame * 16 * 3]
+              end
             end
-            train_data.push [array[1], Jubatus::Common::Datum.new(hash)]
+            train_data.push [array[1], Jubatus::Common::Datum.new(mfcc)]
         }
+        end
     end
 end
 
@@ -30,4 +44,3 @@ end
 train_data.sort_by{rand}
 
 client.train(train_data)
-
